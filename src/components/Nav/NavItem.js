19dd180ce7fe6c47acf290fc2a34b57c6033f836
strahinja <strahinja.jkc@gmail.com>
2021-01-26 23:43:02 +0100

    add transition duration for nav items, center text

--- src/components/Nav/NavItem.js
+++ src/components/Nav/NavItem.js
@@ -4,7 +4,7 @@ import {setActive} from '../../reducers/activeTabReducer'
 import {Link} from 'react-router-dom'
 import {StyleSheet, css} from 'aphrodite'
 import {displayLogin} from '../../reducers/loginReducer'
-
+import {useSpring, animated} from 'react-spring'
 
 const NavItem = ({text, color}) => {
     const styles = StyleSheet.create({
@@ -12,14 +12,25 @@ const NavItem = ({text, color}) => {
         height: '30px',
         width: '30px',
         backgroundColor: color,
-        borderRadius: '100px'
+        borderRadius: '100px',
+        transitionDuration: '0.5s',
+        color: '#ffff',
+        textAlign: 'center'
+
     },
     hoverItem: {
+        transitionDuration: '0.5s',
         height: '30px',
         width: '100px',
         color: '#ffff',
         borderRadius: '20px',
         backgroundColor: color,
+        textAlign: 'center'
+    },
+    textStyle: {
+        display: 'inline-block',
+        lineHeight: '30px',
+        verticalAlign: 'middle'
     }
 })
 
@@ -35,9 +46,9 @@ const NavItem = ({text, color}) => {
     }
 
     return(
-        <Link role='nav-link' onMouseOver={() => setHovered(true)} onMouseLeave={() => setHovered(false)} id={text.toLowerCase()} onClick={handleClick} to={`/${text}`}>
+        <Link style={{textDecoration: 'none'}} onMouseOver={() => setHovered(true)} onMouseLeave={() => setHovered(false)} id={text.toLowerCase()} onClick={handleClick} to={`/${text}`}>
             <div  className={css( hovered ? styles.hoverItem : styles.navItem)}>
-                {hovered ? text : ''}
+                <span className={css(styles.textStyle)}>{hovered ? text : ''}</span>
             </div>
         </Link>
 
