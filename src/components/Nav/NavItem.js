6173f66f644c37a005263a3151ae5709ecccec14
strahinja <strahinja.jkc@gmail.com>
2021-01-27 19:29:49 +0100

    default color of nav font to background color

--- src/components/Nav/NavItem.js
+++ src/components/Nav/NavItem.js
@@ -30,12 +30,12 @@ const NavItem = ({text, passedColor}) => {
     textStyle: {
         display: 'inline-block',
         lineHeight: '30px',
-        verticalAlign: 'middle'
+        verticalAlign: 'middle',
+
     }
 })  
 
-    const startColor = passedColor.substring(0, passedColor.lastIndexOf(')')) + ', 0)'
-    console.log(startColor)
+    const startColor = 'rgba(249,249,249,0)' 
     const dispatch = useDispatch()
     const [hovered, setHovered] = useState(false)
     const a = {color: startColor, config: {duration: 500} }
@@ -48,7 +48,7 @@ const NavItem = ({text, passedColor}) => {
         dispatch(displayLogin())
     }
     return(
-        <Link         onMouseEnter={() => set({color: "#fff" })}
+        <Link         onMouseEnter={() => set({color: "rgba(249,249,249,1)" })}
         onMouseLeave={() => {set({color: startColor }); setHovered(false)}} style={{textDecoration: 'none'}} onMouseOver={() => setHovered(true)}  id={text.toLowerCase()} onClick={handleClick} to={`/${text}`}>
             <div  className={css( hovered ? styles.hoverItem : styles.navItem)}>
                 <animated.span  style={obj} className={css(styles.textStyle)}>{hovered ? text : ''}</animated.span>
