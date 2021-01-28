f21231d6ab82d6b77c6ca8196b21a605c60de255
strahinja <strahinja.jkc@gmail.com>
2021-01-28 17:53:45 +0100

    ad fadein to overview images

--- src/components/ImageComponent.js
+++ src/components/ImageComponent.js
@@ -2,6 +2,7 @@ import React,{useState} from 'react'
 import {StyleSheet, css} from 'aphrodite'
 import {setPhoto} from '../reducers/fullImageReducer'
 import {useDispatch} from 'react-redux'
+import { useSpring, animated } from 'react-spring'
 
 const styles = StyleSheet.create({
     imageContainer: {
@@ -9,7 +10,6 @@ const styles = StyleSheet.create({
         overflow: 'hidden',
         margin: '50px 0 50px 0',
         boxSizing: 'border-box',
-        transition: 'ease-in 400ms',
         borderRadius: '10px',
         boxShadow: '0px 4px 15px 2px rgba(0,0,0,0.15)'
     },
@@ -17,7 +17,6 @@ const styles = StyleSheet.create({
         width: '100%',
         height: '100%',
         display: 'block',
-        transition: '400ms'
     },
     imageHover: {
             transform: 'scale(1.08)',
@@ -27,17 +26,25 @@ const styles = StyleSheet.create({
 })
 
 const ImageComponent = ({photo, children}) => {
+    const fadeIn = useSpring({opacity: 1, from: {opacity: 0.3}, config: {duration: 500}})
+    const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }))
     const [containerHovered, setContainerHovered] = useState(false)
     const dispatch = useDispatch()
+    const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.07]
+    const trans = (x, y, s) => `perspective(2000px)  scale(${s})`
 
     const handleImageClick = () => {
         dispatch(setPhoto(photo))    
     }
+    
     return(
-        <div onMouseOver={() => setContainerHovered(true)} onMouseLeave={() => setContainerHovered(false)} className={css(styles.imageContainer)}>
+        <animated.div class="card"
+        onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+        onMouseLeave={() => set({ xys: [0, 0, 1] })}
+        style={{ transform: props.xys.interpolate(trans) }} className={css(styles.imageContainer)}>
             {children}
-            <img className={containerHovered ? css(styles.imageHover, styles.image) : css(styles.image)} src={photo.small} alt='overview' onClick={handleImageClick}></img>
-        </div>
+            <animated.img style={fadeIn} className={css(styles.image)} src={photo.small} alt='overview' onClick={handleImageClick}></animated.img>
+        </animated.div>
     )
 }
 
