3970d92c8a5102be0895f7b92a05aabc71318123
strahinja <strahinja.jkc@gmail.com>
2021-01-31 22:46:52 +0100

    redesign equipment cards, insert test dots

--- src/components/Equipment/EquipmentItem.js
+++ src/components/Equipment/EquipmentItem.js
@@ -1,47 +1,70 @@
 import React from 'react'
 import {StyleSheet, css} from 'aphrodite'
+import NavItem from '../Nav/NavItem'
 
 const styles = StyleSheet.create({
     elementContainer: {
+        width: '64%',
+        height: '274px',
+        margin: '0px 0 0 0',
         display: 'flex',
-        justifyContent: 'space-around',
+        justifyContent: 'center',
         alignItems: 'center',
         overflow: 'hidden',
-        marginBottom: '10px'
+        boxShadow: '0px 4px 15px 2px rgba(0,0,0,0.15)',
+        borderRadius: '10px'
     },
     imageContainer: {
-        justifySelf: 'center',
-        alignSelf: 'center',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
         height: '100%',
         width: '50%',
-        backgroundColor: '#333630',
-        padding: '5px',
-        boxSizing: 'border-box'
+        borderRadius: '10px',
     },
     image: {
-        width: '100%',
-        height: '100%',
+        width: '80%',
+        height: '80%',
     },
     text: {
-        width: '40%',
-        textAlign: 'justify',
-        justifySelf: 'center',
-        alignSelf: 'center',
-        color: '#E5E5E5',
-        backgroundColor: '#1F201D',
-        padding: '10px',
-        borderRadius: '5px'
+        color: '#fff',
+        textAlign: 'justify'
+    },
+    textContainer: {
+        display:'flex',
+        height: '100%',
+        width: '50%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgb(39, 187, 229)',
+        padding: '10px'
+    },
+    dotsWrapper:{
+        height: '200px',
+        display:'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-around'
     }
 })
 
 const EquipmentItem = ({img, alt, text}) => {
     return (
-        <div className={css(styles.elementContainer)}>
-            <div className={css(styles.imageContainer)}>
-                <img className={css(styles.image)} src={img} alt={alt}></img>
+        <>
+            <div className={css(styles.elementContainer)}>
+                <div className={css(styles.imageContainer)}>
+                    <img className={css(styles.image)} src={img} alt={alt}></img>
+                </div>
+                <div className={css(styles.textContainer)}>
+                    <p className={css(styles.text)}>{text}</p>
+                </div>
             </div>
-            <p className={css(styles.text)}>{text}</p>
-        </div>
+            <div className={css(styles.dotsWrapper)}>
+                <NavItem passedColor={'rgba(39, 187, 229)'} text={''} button={false} />
+                <NavItem passedColor={'rgba(39, 187, 229)'} text={''} button={false} />
+                <NavItem passedColor={'rgba(39, 187, 229)'} text={''} button={false} />
+                <NavItem passedColor={'rgba(39, 187, 229)'} text={''} button={false} />
+            </div>
+        </>
     )
 }
 
